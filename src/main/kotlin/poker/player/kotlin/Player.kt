e68889f557d6fd8c4c8cfd049bb867f6fb95092d
Benjamin Müller <ben@getbring.com>
2025-09-10 09:27:51 +0200

    every player has placed no bets we bet the small blind

--- src/main/kotlin/poker/player/kotlin/Player.kt
+++ src/main/kotlin/poker/player/kotlin/Player.kt
@@ -3,8 +3,40 @@ package poker.player.kotlin
 import org.json.JSONObject
 
 class Player {
+    val numberOfPlayers = 4
     fun betRequest(game_state: JSONObject): Int {
+        val currentSmallBlind = game_state.getInt("small_blind")
+
+        val indexOfDealer = game_state.getInt("dealer")
+        val round = game_state.getInt("round")
+
+        val ourPlayer = getOurPlayer(game_state)
+        println(ourPlayer)
+
+        val ourIndex = ourPlayer.getInt("id")
+        println(ourIndex)
+
+        if (everyPlayersBetIsZero(game_state)) {
+            return game_state.getInt("small_blind")
+        }
+
+
         return 0
+    }
+
+    private fun everyPlayersBetIsZero(gameState: JSONObject): Boolean {
+        val filter = gameState.getJSONArray("players").filter { player ->
+            player is JSONObject && player.getInt("bet") != 0 && player.getString("status").equals("active", true)
+        }
+
+        return filter.isEmpty()
+    }
+
+    private fun getOurPlayer(game_state: JSONObject): JSONObject {
+        val find = game_state.getJSONArray("players").find { player ->
+            player is JSONObject && player.getString("name").equals("the donkey killers", true)
+        }
+        return find as JSONObject
     }
 
     fun showdown() {
